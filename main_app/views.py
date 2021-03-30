871ece1c5cda7cee38742d88962b54b933d28949
John Teckemeyer <johnteckemeyer@gmail.com>
2021-03-29 17:40:51 -0700

    Create Finch model schema and use in main_app.views

--- main_app/views.py
+++ main_app/views.py
@@ -1,26 +1,14 @@
 from django.shortcuts import render
+from .models import Finch
 
 # Create your views here.
-from django.http import HttpResponse
-
-class Finch:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, common_name, description, weight):
-    self.name = name
-    self.common_name = common_name
-    self.description = description
-    self.weight = weight
-
-finches = [
-  Finch('Lolo', 'Hawaiian Honeycreeper', 'Dark red with long, curved bill', 13),
-  Finch('Sachi', 'Rosefinch', 'Red and plump', 24),
-  Finch('Raven', 'Desert Finch', 'yellowish', 25)
-]
 
 def home(request):
-    return HttpResponse('<h1>Hello Finches</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
 def finches_index(request):
+    finches = Finch.objects.all()
     return render(request, 'finches/index.html', { 'finches': finches })
