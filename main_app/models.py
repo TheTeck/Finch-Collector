42ac9672f463bfa9203e1034250f70f3a7427d09
John Teckemeyer <johnteckemeyer@gmail.com>
2021-03-30 21:49:08 -0700

    Add Feeding class to models

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,13 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
 class Finch(models.Model):
     name = models.CharField(max_length=100)
     common_name = models.CharField(max_length=100)
@@ -9,3 +16,18 @@ class Finch(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={ 'finch_id': self.id })
+
+class Feeding(models.Model):
+    date = models.DateField()
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0]
+    )
+    finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_meal_diplay()} on {self.date}"
